123bc90983d3f1d71471e3b3f1741768e71eba41
eternallycyf <969475322@qq.com>
2023-08-31 17:19:38 +0800

    feat: add variables

--- .umirc.ts
+++ .umirc.ts
@@ -2,20 +2,41 @@ import { defineConfig } from '@umijs/max';
 import extraPostCSSPlugins from './config/extraPostCSSPlugins';
 const routerConfig = require('./src/routes');
 const proxyConfig = require('./src/config/proxyConfig');
+const path = require('path');
 
 export default defineConfig({
   define: {
     'process.env': process.env,
   },
-  base: process.env.APP_ENV === 'development' ? '/' : '/ims-app/',
-  publicPath: process.env.APP_ENV === 'development' ? '/' : '/ims-app/',
+  base: process.env.NODE_ENV === 'development' ? '/' : '/ims-app/',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/ims-app/',
+  layout: false,
+  codeSplitting: {
+    jsStrategy: 'granularChunks',
+  },
+  fastRefresh: true,
+  clickToComponent: {},
+  devtool: process.env.NODE_ENV === 'production' ? 'eval' : 'source-map',
   hash: true,
+  mfsu: true,
   routes: routerConfig,
   proxy: proxyConfig,
   extraPostCSSPlugins, // px转换为vw
+  dva: {},
   access: {},
   model: {},
   initialState: {},
   request: {},
+  lessLoader: {
+    modifyVars: {
+      hack: `true; @import "${path.resolve(
+        'src/assets/styles/variable.less',
+      )}";`,
+    },
+    javascriptEnabled: true,
+  },
+  cssLoader: {
+    url: true,
+  },
   npmClient: 'yarn',
 });
